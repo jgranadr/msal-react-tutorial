3bf780cff5a8260838418812a1a3c44bba189d5a
jgranadr <jgranadr@everis.com>
2022-05-04 10:06:08 +0200

    conditional rendering based on authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { PageLayout } from "./components/PageLayout";
-
+import { AuthenticatedTemplate, UnauthenticatedTemplate } from "@azure/msal-react";
 function App() {
   return (
       <PageLayout>
-          <p>This is the main app content!</p>
+          <AuthenticatedTemplate>
+              <p>You are signed in!</p>
+          </AuthenticatedTemplate>
+          <UnauthenticatedTemplate>
+              <p>You are not signed in! Please sign in.</p>
+          </UnauthenticatedTemplate>
       </PageLayout>
   );
 }
