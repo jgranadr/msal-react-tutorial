555e7e9b4359a772c9e32a029c68a7805c6ff85c
jgranadr <jgranadr@everis.com>
2022-05-04 10:14:31 +0200

    Acquire a Token

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import React from "react";
+
+import React, { useState } from "react";
 import { PageLayout } from "./components/PageLayout";
-import { AuthenticatedTemplate, UnauthenticatedTemplate } from "@azure/msal-react";
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import { loginRequest } from "./authConfig";
+import Button from "react-bootstrap/Button";
+
+function ProfileContent() {
+  const { instance, accounts, inProgress } = useMsal();
+  const [accessToken, setAccessToken] = useState(null);
+
+  const name = accounts[0] && accounts[0].name;
+
+  function RequestAccessToken() {
+      const request = {
+          ...loginRequest,
+          account: accounts[0]
+      };
+
+      // Silently acquires an access token which is then attached to a request for Microsoft Graph data
+      instance.acquireTokenSilent(request).then((response) => {
+          setAccessToken(response.accessToken);
+      }).catch((e) => {
+          instance.acquireTokenPopup(request).then((response) => {
+              setAccessToken(response.accessToken);
+          });
+      });
+  }
+
+  return (
+      <>
+          <h5 className="card-title">Welcome {name}</h5>
+          {accessToken ? 
+              <p>Access Token Acquired!</p>
+              :
+              <Button variant="secondary" onClick={RequestAccessToken}>Request Access Token</Button>
+          }
+      </>
+  );
+};
 function App() {
   return (
       <PageLayout>
           <AuthenticatedTemplate>
-              <p>You are signed in!</p>
+              <ProfileContent />
           </AuthenticatedTemplate>
           <UnauthenticatedTemplate>
               <p>You are not signed in! Please sign in.</p>
@@ -13,5 +50,4 @@ function App() {
       </PageLayout>
   );
 }
-
 export default App;
